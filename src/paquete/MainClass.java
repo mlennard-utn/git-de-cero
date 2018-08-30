1a244c2d514e84ce8b86dd69c471354dda1201ad
mlennard-utn <39415012+mlennard-utn@users.noreply.github.com>
2018-08-29 21:45:44 -0300

    Sigue la conversación en sysout

--- src/paquete/MainClass.java
+++ src/paquete/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Chau Mundo!"); 
 		System.out.println("Como estas hoy?");
+		System.out.println("Bien y vos?");
 	}
 }
